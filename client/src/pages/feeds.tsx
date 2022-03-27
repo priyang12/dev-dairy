021f062603cc718c828be4da767cdb8232e24a6f
Priyang <patelpriyang95@gmail.com>
2022-03-27 23:52:04 +0530

    Mocked Posts

--- client/src/pages/feeds.tsx
+++ client/src/pages/feeds.tsx
@@ -1,81 +1,21 @@
 import { Fragment, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import FeedItem from '../components/feedsItems';
-import Send from '../components/send';
-import { GetPosts } from '../actions/PostAction';
+import PostContainer from '../components/PostContainer';
 import Spinner from '../components/spinner';
+import { getPostsAction } from '../actions/PostAction';
 
 function Feeds() {
   const { loading } = useSelector((state: any) => state.Alert);
   const { posts } = useSelector((state: any) => state.Post);
   const dispatch = useDispatch();
-  // console.log(posts[0]);
-  // useEffect(() => {
-  //   if (posts.length === 0) {
-  //     dispatch(GetPosts());
-  //   }
-  // }, [posts, dispatch]);
-
-  const Faked = [
-    {
-      date: 1642350791639,
-      name: 'priyang',
-      text: 'New Post asdsad as asdihasd asdhasi',
-      title: 'New Post',
-      user: '3d5idu5RcuYWKkuuv5jVfrZHx8e2',
-    },
-    {
-      date: 1642351238323,
-      text: 'New Post asdsad as asdihasd asdhasi',
-      title: 'Test the Post',
-      user: '3d5idu5RcuYWKkuuv5jVfrZHx8e2',
-    },
-    {
-      date: 1642351262688,
-      text: 'New Post asdsad as asdihasd asdhasi',
-      title: 'Test the Post',
-      user: '3d5idu5RcuYWKkuuv5jVfrZHx8e2',
-    },
-    {
-      date: 1642352904763,
-      text: 'New Post asdsad as asdihasd asdhasi',
-      title: 'Test the Post',
-      user: '3d5idu5RcuYWKkuuv5jVfrZHx8e2',
-    },
-    {
-      date: 1642352967556,
-      text: 'New Post asdsad as asdihasd asdhasi',
-      title: 'Test the Post',
-      user: '3d5idu5RcuYWKkuuv5jVfrZHx8e2',
-    },
-    {
-      date: 1642352985139,
-      text: 'New Post asdsad as asdihasd asdhasi',
-      title: 'Test the Post',
-      user: '3d5idu5RcuYWKkuuv5jVfrZHx8e2',
-    },
-    {
-      date: 1642353014645,
-      text: 'New Post asdsad as asdihasd asdhasi',
-      title: 'Test the Post',
-      user: '3d5idu5RcuYWKkuuv5jVfrZHx8e2',
-    },
-    {
-      date: '2022-01-16T17:13:44.562Z',
-
-      likes: [
-        {
-          user: '3d5idu5RcuYWKkuuv5jVfrZHx8e2',
-        },
-      ],
-      text: 'New Post asdsad as asdihasd asdhasi',
-      title: 'Test the Post',
-      user: '3d5idu5RcuYWKkuuv5jVfrZHx8e2',
-    },
-  ];
+  useEffect(() => {
+    dispatch(getPostsAction());
+  }, []);
+  console.log(posts);
   if (loading) return <Spinner />;
+
   return (
-    <>
+    <Fragment>
       {/* <Send /> */}
       <div className="feed">
         <div className="container">
@@ -87,15 +27,15 @@ function Feeds() {
             </div>
           </div>
           <div className="col p-3">
-            {Faked
-              && Faked.map((post: any, index: number) => (
-                <FeedItem key={index} post={post} />
-              ))}
+            {/* {posts &&
+              posts.map((post: Post) => (
+                <PostContainer key={post.uid} post={post} />
+              ))} */}
           </div>
         </div>
       </div>
       )
-    </>
+    </Fragment>
   );
 }
 
