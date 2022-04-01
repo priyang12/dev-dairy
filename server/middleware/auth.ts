cc0397cca7ad08c145c7a4fc043a73b43e99ea9c
Priyang <patelpriyang95@gmail.com>
2022-04-01 18:14:15 +0530

    Temp
    Changed Auth Middleware for testing

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,21 +1,23 @@
-import type { NextFunction, Request, Response } from 'express';
-import { auth as getAuth } from 'firebase-admin';
+import type { NextFunction, Request, Response } from "express";
+import { auth as getAuth } from "firebase-admin";
+import User from "../models/User";
 
 export default async (req: any, res: Response, next: NextFunction) => {
-  const token = req.header('x-auth-token');
+  const token = req.header("x-auth-token");
   //check token
   if (!token) {
-    return res.status(401).json({ msg: 'no token, authorization denied' });
+    return res.status(401).json({ msg: "no token, authorization denied" });
   }
   try {
-    const decoded = await getAuth().verifyIdToken(token);
+    // const decoded = await getAuth().verifyIdToken(token);
+    // if (decoded.exp < Date.now() / 1000) {
+    //   return res.status(401).json({ msg: "token has expired" });
+    // }
+    const UserID = await User.findOne({ uid: "7IrFCsse9sRIDwscUce4LuEN8ZG3" });
 
-    if (decoded.exp < Date.now() / 1000) {
-      return res.status(401).json({ msg: 'token has expired' });
-    }
-    req.user._id = decoded.uid;
+    req.user = UserID;
     next();
   } catch (error) {
-    res.status(401).json({ msg: 'token is not valid' });
+    res.status(401).json({ msg: "token is not valid" });
   }
 };
