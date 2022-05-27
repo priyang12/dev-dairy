9f635e0a7d9a39fce2c4a2577d0207f099adb3ff
Priyang <patelpriyang95@gmail.com>
2022-05-28 02:06:18 +0530

    New Page Formate

---
+++ client/PageFormate.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+# variables
+Path="src/Pages"
+
+# get names of files in path
+
+# Index Page Boilerplate
+index() {
+    echo "import ${1} from './${1}.tsx';" >>$Path/index.tsx
+    echo "export default ${1};" >>$Path/index.tsx
+}
+
+for entry in "$Path"/*; do
+    echo "$entry"
+    # create a folder for each file and remove .js extension
+    mkdir -p "$Path/$(basename "$entry" .tsx)"
+    touch "$Path/$(basename "$entry" .tsx)/index.tsx"
+    # create a file for each file and remove .js extension
+    mv "$entry" "$Path/$(basename "$entry" .tsx)/$(basename "$entry" .tsx).tsx"
+    # touch "$Path/$(basename "$entry" .tsx)/index.tsx"
+    # index $(basename "$entry" .tsx)
+
+done
