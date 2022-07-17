f49e8d682bd202d08ee3a4d1fb7eda920c3132ac
Priyang <patelpriyang95@gmail.com>
2022-07-17 08:03:20 +0530

    Nav

---
+++ client/src/components/Navbar/NavLink.tsx
@@ -0,0 +1,70 @@
+import { Stack, Box, Link, Flex } from '@chakra-ui/react';
+import { motion } from 'framer-motion';
+import { Link as RouterLink } from 'react-router-dom';
+import DropDownContainer from './DropDownContainer';
+
+function MenuItem({ children, isDark, isLast, to = '/', ...rest }: any) {
+  return (
+    <Link
+      as={RouterLink}
+      to={to}
+      color="white"
+      backgroundColor={isDark ? 'primary.200' : 'primary.600'}
+      border="10px solid"
+      borderColor="primary.900"
+      outline="2px solid white"
+      borderRadius={10}
+      rounded="lg"
+      textAlign="center"
+      padding={2}
+      {...rest}
+    >
+      {children}
+    </Link>
+  );
+}
+
+function NavLinks({ isOpen, isAuth }: { isOpen: boolean; isAuth: boolean }) {
+  return (
+    <Box
+      // Look more
+      display={{ base: isOpen ? 'block' : 'none', md: 'block' }}
+      flexBasis={{ base: '100%', md: 'auto' }}
+      as={motion.div}
+      animate={isOpen ? 'show' : 'hidden'}
+      variants={{
+        hidden: { y: -1000, opacity: 0 },
+        show: { y: 1, opacity: 1 },
+      }}
+    >
+      <Stack
+        spacing={8}
+        align="center"
+        justify={['center', 'space-between', 'flex-end', 'flex-end']}
+        direction={['column', 'column', 'row', 'row']}
+        pt={[4, 4, 0, 0]}
+      >
+        {!isAuth ? (
+          <>
+            <MenuItem to="/login" w={['100%', '50%', 'auto']}>
+              Log In
+            </MenuItem>
+            <MenuItem to="/register" w={['100%', '50%', 'auto']}>
+              Register{' '}
+            </MenuItem>
+          </>
+        ) : (
+          <Flex gap={10} justifyContent="space-around" w="100%">
+            <Flex direction={['column', 'row', 'row']} gap={5}>
+              <MenuItem to="/feeds">Feeds</MenuItem>
+              <MenuItem to="/MusicPlaylist">MusicPlaylist</MenuItem>
+            </Flex>
+            <DropDownContainer />
+          </Flex>
+        )}
+      </Stack>
+    </Box>
+  );
+}
+
+export default NavLinks;
---
+++ client/src/components/Navbar/NewNavbar.tsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import { Link, Box, Flex, Text, Heading, IconButton } from '@chakra-ui/react';
+import { Link as RouterLink, useLocation } from 'react-router-dom';
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+import { useSelector } from 'react-redux';
+import Logo from '../../Assets/diary.png';
+import NavLinks from './NavLink';
+import type { AuthState } from '../../interface';
+
+function StyledIcon({ children, isDark, ...props }: any) {
+  return (
+    <IconButton
+      aria-label="Open Menu"
+      size="lg"
+      bgColor={isDark ? 'blackAlpha.100' : 'blackAlpha.500'}
+      _focus={{ outline: 'none', border: '3px solid teal.600' }}
+      mr={2}
+      display={['flex', 'flex', 'none', 'none']}
+      {...props}
+    >
+      {children}
+    </IconButton>
+  );
+}
+
+function MenuToggle({ toggle, isOpen }: any) {
+  return (
+    <Box display={{ base: 'block', md: 'none' }} onClick={toggle}>
+      {!isOpen ? (
+        <StyledIcon>
+          <CloseIcon />
+        </StyledIcon>
+      ) : (
+        <StyledIcon>
+          <HamburgerIcon />
+        </StyledIcon>
+      )}
+    </Box>
+  );
+}
+
+function NavBarContainer({ children, ...props }: any) {
+  return (
+    <Flex
+      as="nav"
+      align="center"
+      justify="space-between"
+      wrap="wrap"
+      w="100%"
+      p={8}
+      bg="primary.900"
+      {...props}
+    >
+      {children}
+    </Flex>
+  );
+}
+
+function LogoComponent() {
+  return (
+    <Link
+      as={RouterLink}
+      to="/"
+      color="primary.100"
+      _hover={{ color: 'primary.600' }}
+      className="nav-link"
+    >
+      <Flex alignItems="center">
+        <img src={Logo} width={50} alt="Logo" />
+        <Box ml={2}>
+          <Heading fontFamily="cursive">
+            <span className="logo-text">Dairy</span>
+          </Heading>
+        </Box>
+      </Flex>
+    </Link>
+  );
+}
+
+function NavBar() {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+
+  const location = useLocation();
+  const isLogin = location.pathname === '/login';
+  const isRegister = location.pathname === '/register';
+  const LandingPage = location.pathname === '/';
+
+  const { authenticated }: AuthState = useSelector((state: any) => state.Auth);
+
+  if (LandingPage || isLogin || isRegister) return null;
+
+  return (
+    <NavBarContainer>
+      <LogoComponent />
+      <MenuToggle toggle={toggle} isOpen={isOpen} />
+      <NavLinks isOpen={!isOpen} isAuth={authenticated} />
+    </NavBarContainer>
+  );
+}
+
+export default NavBar;
