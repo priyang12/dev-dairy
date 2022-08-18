3ea72a966a1b1c187e8a065829a1509f6a45e36f
Priyang <patelpriyang95@gmail.com>
2022-08-18 19:30:12 +0530

    filter

--- client/src/pages/FilterPosts/FilterPosts.test.tsx
+++ client/src/pages/FilterPosts/FilterPosts.test.tsx
@@ -1,7 +1,30 @@
-import { render } from '../../test-utils';
+import { render, screen, waitForElementToBeRemoved } from '../../test-utils';
 import '@testing-library/jest-dom/extend-expect';
 import FilterPosts from './FilterPosts';
+import { BrowserRouter } from 'react-router-dom';
+import { PostsResponse } from '../../mock/MockedData';
+import { format, parseISO } from 'date-fns';
 
-it('should render without crashing', () => {
-  render(<FilterPosts />);
+const setup = (): any => {
+  render(
+    <BrowserRouter>
+      <FilterPosts />
+    </BrowserRouter>,
+  );
+};
+
+it('render Posts', async () => {
+  setup();
+  expect(screen.getByAltText('loading...')).toBeInTheDocument();
+  await waitForElementToBeRemoved(screen.queryByAltText('loading...'), {
+    timeout: 2100,
+  });
+  expect(screen.getByText('Filter Log')).toBeInTheDocument();
+  PostsResponse.forEach((post) => {
+    expect(screen.getByText(post.title)).toBeInTheDocument();
+    expect(screen.getByText(post.description)).toBeInTheDocument();
+    expect(
+      screen.getByText(format(parseISO(post.date), "yyyy-MM-dd' 'HH:mm")),
+    ).toBeInTheDocument();
+  });
 });
