0018a4e8050fee3e8731e150999c9530df1b8d18
Priyang <patelpriyang95@gmail.com>
2022-08-31 08:29:59 +0530

    Fixed LoadMore posts
    With updating the cache with some workarounds need to better or other way to find way for updating and mange.

--- client/src/Hooks/useInfinitePosts.ts
+++ client/src/Hooks/useInfinitePosts.ts
@@ -1,10 +1,13 @@
-import { useRef, useState, useEffect, useMemo } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import { useLazyGetPosts } from '../API/PostAPI';
+import { IPost } from '../interface';
 
 export const useInfinitePosts = () => {
   const shouldReset = useRef(true);
   const [page, setpage] = useState(2);
   const [trigger, result] = useLazyGetPosts();
+  const [posts, setPosts] = useState<IPost[]>([]);
+  const [postsIds, setPostsIds] = useState<Set<string>>(new Set());
   const [End, setEnd] = useState(false);
 
   useEffect(() => {
@@ -16,23 +19,62 @@ export const useInfinitePosts = () => {
     trigger({ page: 1, limit: 10 });
   }, []);
 
-  const combined = useMemo(() => {
-    const arr = new Array(10 * (page + 1));
+  useEffect(() => {
+    if (!result.isSuccess) return;
     if (result.data?.length === 0) setEnd(true);
-
-    for (const data of [result.data]) {
-      if (data) {
-        for (const item of data) {
-          arr.push(item);
+    if (shouldReset.current) {
+      // setPosts(new Set(result.data));
+      setPosts(result.data);
+      const ids = new Set(result.data.map((post) => post._id));
+      setPostsIds(ids);
+      shouldReset.current = false;
+    } else if (result.data && result.data?.length > 0) {
+      const NewPosts = [...posts];
+      const ids = new Set(postsIds);
+      result.data.forEach((post) => {
+        if (ids.has(post._id)) {
+          NewPosts[NewPosts.findIndex((p) => p._id === post._id)] = post;
+        } else {
+          ids.add(post._id);
+          NewPosts.push(post);
         }
-      }
+      });
+      setPostsIds(ids);
+      setPosts(NewPosts);
     }
-    return arr;
-  }, [result]);
+  }, [result.data]);
+
+  const DeletePost = (id: string) => {
+    const NewPosts = [...posts];
+    const NewPostsIds = new Set(postsIds);
+    NewPostsIds.delete(id);
+    NewPosts.splice(
+      NewPosts.findIndex((p) => p._id === id),
+      1,
+    );
+    setPostsIds(NewPostsIds);
+    setPosts(NewPosts);
+  };
+  // const UpdatePost = (post: IPost) => {
+  //   const NewPosts = [...posts];
+  //   const NewPostsIds = new Set(postsIds);
+  //   NewPostsIds.delete(post._id);
+  //   NewPosts.splice(NewPosts.findIndex((p) => p._id === post._id), 1, post);
+  //   setPostsIds(NewPostsIds);
+  //   setPosts(NewPosts);
+  // }
+  // const AddPost = (post: IPost) => {
+  //   const NewPosts = [...posts];
+  //   const NewPostsIds = new Set(postsIds);
+  //   NewPostsIds.add(post._id);
+  //   NewPosts.push(post);
+  //   setPostsIds(NewPostsIds);
+  //   setPosts(NewPosts);
+  // }
 
   return {
     ...result,
-    data: combined,
+    data: posts,
     CurrentPage: page,
     isLastPage: End,
     fetchNextPage() {
@@ -41,5 +83,6 @@ export const useInfinitePosts = () => {
         setpage((prevPage: number) => prevPage + 1);
       }
     },
+    DeletePost,
   };
 };
