82944cb984836edbabbbad3707dcf69c3a9cb354
Priyang <patelpriyang95@gmail.com>
2025-07-31 11:24:45 +0530

    importing assets

--- client/src/components/spinner.tsx
+++ client/src/components/spinner.tsx
@@ -1,9 +1,9 @@
-/* eslint-disable */
+import Loading from '../Assets/loading.gif';
 
 export default () => (
   <div className="top">
     <img
-      src={require('../Assets/loading.gif')}
+      src={Loading}
       style={{ width: '200px', margin: 'auto', display: 'block' }}
       alt="loading..."
     />
